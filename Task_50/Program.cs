77f73d498890532a55c943823c04a6100f254536
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-11-27 05:39:26 +0200

    Added task 50

---
+++ Task_50/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном
+//  массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+bool IsParsedM = int.TryParse(Console.ReadLine(), out int m);
+bool IsParsedN = int.TryParse(Console.ReadLine(), out int n);
+if (!IsParsedM || !IsParsedN)
+{
+    Console.WriteLine("Error!");
+    return;
+}
+
+int[,] matrix = new int[m, n];
+FillMatrix(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Введите позиции элемента:");
+int i = Convert.ToInt16(Console.ReadLine());
+int j = Convert.ToInt16(Console.ReadLine());
+if (i < m && j < m)
+{
+    Console.WriteLine(matrix[i,j]);
+}
+else
+{
+    Console.WriteLine("Number with such indexex does not exist!");
+}
+
+
+void FillMatrix(int[,] m)
+{
+    Random random = new Random();
+    for (int i = 0; i < m.GetLength(0); i++)
+    {
+        for (int j = 0; j < m.GetLength(1); j++)
+        {
+            m[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // возвращает кол-во строк в массиве
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // возвращает кол-во столбцов в массиве  
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
